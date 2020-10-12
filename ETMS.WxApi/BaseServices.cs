8eab7f53c67a8f83ea14f54bc9e79f85ca772be0
chenjian8541 <chenjian8541@126.com>
2020-10-12 15:20:31 +0800

    微信

---
+++ ETMS.WxApi/BaseServices.cs
@@ -0,0 +1,27 @@
+﻿using ETMS.Utility;
+using System;
+
+namespace ETMS.WxApi
+{
+    public class BaseServices
+    {
+        /// <summary>
+        /// 接入验证
+        /// </summary>
+        /// <param name="token"></param>
+        /// <returns></returns>
+        public static bool ValidUrl(string token, string signature, string timestamp, string nonce, string echostr)
+        {
+            string[] temp = { token, timestamp, nonce };
+            Array.Sort(temp);
+            var tempstr = string.Join("", temp);
+            var tempsign = CryptogramHelper.EncryptSHA1(tempstr).ToLower();
+            LOG.Log.Debug($"[验证消息的确来自微信服务器]tempsign:{tempsign}", typeof(BaseServices));
+            if (tempsign == signature)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
