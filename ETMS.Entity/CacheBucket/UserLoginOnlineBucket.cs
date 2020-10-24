029c57af5b81eb413f9dd94256405312e2176277
chenjian8541 <chenjian8541@126.com>
2020-10-24 22:08:39 +0800

    登陆

---
+++ ETMS.Entity/CacheBucket/UserLoginOnlineBucket.cs
@@ -0,0 +1,40 @@
+﻿using ETMS.Entity.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETMS.Entity.CacheBucket
+{
+    public class UserLoginOnlineBucket : ICacheDataContract
+    {
+        /// <summary>
+        /// 机构ID
+        /// </summary>
+        public int TenantId { get; set; }
+
+        /// <summary>
+        /// 用户ID
+        /// </summary>
+        public long UserId { get; set; }
+
+        /// <summary>
+        /// 登录的时间戳
+        /// </summary>
+        public string LoginTime { get; set; }
+
+        /// <summary>
+        /// timeout时间
+        /// </summary>
+        public TimeSpan TimeOut { get; set; } = TimeSpan.FromDays(BucketTimeOutConfig.UserLoginOnlineDay);
+
+        /// <summary>
+        /// 机构+用户ID
+        /// </summary>
+        /// <param name="parms"></param>
+        /// <returns></returns>
+        public string GetKeyFormat(params object[] parms)
+        {
+            return $"UserLoginOnlineBucket_{parms[0]}_{parms[1]}";
+        }
+    }
+}
