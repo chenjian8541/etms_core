3e6396f6fe92adc8dc41c107892dd89a72b5d45f
chenjian8541 <chenjian8541@126.com>
2020-12-01 09:59:08 +0800

    学员课时不足续费提醒

---
+++ ETMS.Manage.Jobs/NoticeStudentsOfCourseNotEnoughJob.cs
@@ -0,0 +1,33 @@
+﻿using ETMS.Event.DataContract;
+using ETMS.IDataAccess.EtmsManage;
+using ETMS.IEventProvider;
+using ETMS.Manage.Entity.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETMS.Manage.Jobs
+{
+    public class NoticeStudentsOfCourseNotEnoughJob : BaseJob
+    {
+        private readonly ISysTenantDAL _sysTenantDAL;
+
+        private readonly IEventPublisher _eventPublisher;
+
+        public NoticeStudentsOfCourseNotEnoughJob(ISysTenantDAL sysTenantDAL, IEventPublisher eventPublisher)
+        {
+            this._sysTenantDAL = sysTenantDAL;
+            this._eventPublisher = eventPublisher;
+        }
+
+        public override async Task Process(JobExecutionContext context)
+        {
+            var tenantList = await _sysTenantDAL.GetTenantsNormal();
+            foreach (var tenant in tenantList)
+            {
+                _eventPublisher.Publish(new TenantStudentCourseNotEnoughEvent(tenant.Id));
+            }
+        }
+    }
+}
