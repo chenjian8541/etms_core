a724c1bfe0469f6e542d2b23ee6c9d421f6cf98f
chenjian8541 <chenjian8541@126.com>
2020-12-28 11:32:39 +0800

    #bug6599 需求:学员信息可以通过名字模糊查询，比如小禾帮，输入禾帮可以查询，现在只能输入小查询

--- ETMS.Entity/Dto/Student/Request/StudentGetPagingRequest.cs
+++ ETMS.Entity/Dto/Student/Request/StudentGetPagingRequest.cs
@@ -160,7 +160,7 @@ namespace ETMS.Entity.Dto.Student.Request
             }
             if (!string.IsNullOrEmpty(StudentKey))
             {
-                condition.Append($" AND (Name LIKE '{StudentKey}%' OR Phone LIKE '{StudentKey}%' OR PhoneBak LIKE '{StudentKey}%')");
+                condition.Append($" AND (Name LIKE '%{StudentKey}%' OR Phone LIKE '%{StudentKey}%' OR PhoneBak LIKE '%{StudentKey}%')");
             }
             if (TrackStatus != null)
             {
