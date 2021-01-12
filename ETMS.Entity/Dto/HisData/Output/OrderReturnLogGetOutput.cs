bdc30424ac6331693298279a557e8c6f98018f51
chenjian8541 <chenjian8541@126.com>
2021-01-12 16:10:31 +0800

    退单

---
+++ ETMS.Entity/Dto/HisData/Output/OrderReturnLogGetOutput.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETMS.Entity.Dto.HisData.Output
+{
+    public class OrderReturnLogGetOutput
+    {
+        public long CId { get; set; }
+
+        /// <summary>
+        /// 学员ID
+        /// </summary>
+        public long StudentId { get; set; }
+
+        public int OrderType { get; set; }
+
+        /// <summary>
+        /// 支出类型 <see cref="ETMS.Entity.Enum.EmOrderInOutType"/>
+        /// </summary>
+        public byte InOutType { get; set; }
+
+        /// <summary>
+        /// 单号
+        /// </summary>
+        public string No { get; set; }
+
+        /// <summary>
+        /// 订单总金额
+        /// </summary>
+        public decimal Sum { get; set; }
+
+        /// <summary>
+        /// 应收金额
+        /// </summary>
+        public decimal AptSum { get; set; }
+
+        /// <summary>
+        /// 获得积分
+        /// </summary>
+        public int TotalPoints { get; set; }
+
+        /// <summary>
+        /// 积分
+        /// </summary>
+        public string TotalPointsDesc { get; set; }
+
+        /// <summary>
+        /// 经办人
+        /// </summary>
+        public long UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// 经办日期
+        /// </summary>
+        public string OtDesc { get; set; }
+
+        public DateTime CreateOt { get; set; }
+
+        /// <summary>
+        /// 状态 <see cref="ETMS.Entity.Enum.EmOrderStatus"/>
+        /// </summary>
+        public byte Status { get; set; }
+
+        public string StatusDesc { get; set; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        public string Remark { get; set; }
+
+        public List<OrderReturnLogDetail> LogDetails { get; set; }
+    }
+
+    public class OrderReturnLogDetail {
+
+        public long CId { get; set; }
+
+        public string ProductTypeDesc { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string OutQuantity { get; set; }
+
+        public decimal ItemSum { get; set; }
+
+        public decimal ItemAptSum { get; set; }
+    }
+}
