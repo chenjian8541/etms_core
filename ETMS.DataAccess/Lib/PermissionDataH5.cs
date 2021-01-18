f19b06c5c5dbcdcbd6f2770c97c4ce2bfc5d60e2
chenjian8541 <chenjian8541@126.com>
2021-01-18 18:37:16 +0800

    老师端权限

---
+++ ETMS.DataAccess/Lib/PermissionDataH5.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETMS.DataAccess.Lib
+{
+    public class PermissionDataH5
+    {
+        private static List<int> _pageAllList;
+
+        private static List<int> _actionAllList;
+
+        public static List<int> PageAllList
+        {
+            get
+            {
+                return _pageAllList;
+            }
+        }
+
+        public static List<int> ActionAllList
+        {
+            get
+            {
+                return _actionAllList;
+            }
+        }
+
+        static PermissionDataH5()
+        {
+            _pageAllList = new List<int>();
+            _pageAllList.Add(2);
+            _pageAllList.Add(67);
+            _actionAllList = new List<int>();
+            _actionAllList.Add(1);
+            _actionAllList.Add(2);
+            _actionAllList.Add(4);
+        }
+    }
+}
