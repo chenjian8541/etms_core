c8e10e777bfa1100c5d263f13916799da4812cf9
chenjian8541 <chenjian8541@126.com>
2021-04-21 15:52:09 +0800

    优化

---
+++ ETMS.Entity/CacheBucket/RedisLock/SyncClassInfoConsumerToken.cs
@@ -0,0 +1,27 @@
+﻿using ETMS.Entity.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETMS.Entity.CacheBucket.RedisLock
+{
+    public class SyncClassInfoConsumerToken : IRedisToken
+    {
+        public SyncClassInfoConsumerToken(int tenantId, long classId)
+        {
+            this.TenantId = tenantId;
+            this.ClassId = classId;
+        }
+
+        public int TenantId { get; set; }
+
+        public long ClassId { get; set; }
+
+        public TimeSpan TimeOut { get; set; } = TimeSpan.FromSeconds(20);
+
+        public string GetKey()
+        {
+            return $"SyncClassInfoConsumerToken_{TenantId}_{ClassId}";
+        }
+    }
+}
---
+++ ETMS.Entity/CacheBucket/RedisLock/SyncClassTimesStudentConsumerToken.cs
@@ -0,0 +1,27 @@
+﻿using ETMS.Entity.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ETMS.Entity.CacheBucket.RedisLock
+{
+    public class SyncClassTimesStudentConsumerToken: IRedisToken
+    {
+        public SyncClassTimesStudentConsumerToken(int tenantId, long classTimesId)
+        {
+            this.TenantId = tenantId;
+            this.ClassTimesId = classTimesId;
+        }
+
+        public int TenantId { get; set; }
+
+        public long ClassTimesId { get; set; }
+
+        public TimeSpan TimeOut { get; set; } = TimeSpan.FromSeconds(20);
+
+        public string GetKey()
+        {
+            return $"SyncClassTimesStudentConsumerToken_{TenantId}_{ClassTimesId}";
+        }
+    }
+}
