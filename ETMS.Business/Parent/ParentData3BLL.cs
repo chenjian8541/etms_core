efafbff9a0e75f0b5218ebb63d8cd8dcb35badb0
chenjian8541 <chenjian8541@126.com>
2021-08-11 22:30:23 +0800

    优化

--- ETMS.Business/Parent/ParentData3BLL.cs
+++ ETMS.Business/Parent/ParentData3BLL.cs
@@ -890,6 +890,8 @@ namespace ETMS.Business
                 OpType = NoticeStudentReservationOpType.Success
             });
 
+            _eventPublisher.Publish(new SyncClassInfoEvent(request.LoginTenantId, classTimes.ClassId));
+
             await _studentOperationLogDAL.AddStudentLog(request.StudentId, request.LoginTenantId, $"预约上课-班级:{etClass.EtClass.Name},课次:{classTimes.ClassOt.EtmsToDateString()}({EtmsHelper.GetTimeDesc(classTimes.StartTime, classTimes.EndTime)})", EmStudentOperationLogType.StudentReservation);
             return ResponseBase.Success();
         }
@@ -946,6 +948,8 @@ namespace ETMS.Business
                 ClassTimesStudent = reservationLog,
                 OpType = NoticeStudentReservationOpType.Cancel
             });
+
+            _eventPublisher.Publish(new SyncClassInfoEvent(request.LoginTenantId, classTimes.ClassId));
 
             await _studentOperationLogDAL.AddStudentLog(request.StudentId, request.LoginTenantId, $"取消约课-班级:{etClass.EtClass.Name}，课次:{classTimes.ClassOt.EtmsToDateString()}({EtmsHelper.GetTimeDesc(classTimes.StartTime, classTimes.EndTime)})", EmStudentOperationLogType.StudentReservation);
             return ResponseBase.Success();
