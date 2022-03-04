6d2ab56599bc4cc710c36b241ab3dd31ad8f2a98
chenjian8541 <364905438@qq.com>
2022-03-04 16:20:30 +0800

    定时删除无效的云存储

---
+++ ETMS.Manage.Jobs/CloudFileAutoClearJob.cs
@@ -0,0 +1,48 @@
+﻿using ETMS.Entity.Database.Manage;
+using ETMS.Entity.Database.Source;
+using ETMS.Entity.Enum;
+using ETMS.Entity.Temp;
+using ETMS.Event.DataContract;
+using ETMS.IBusiness;
+using ETMS.IDataAccess;
+using ETMS.IDataAccess.EtmsManage;
+using ETMS.IEventProvider;
+using ETMS.Manage.Entity.Config;
+using ETMS.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETMS.Manage.Jobs
+{
+    public class CloudFileAutoClearJob : BaseTenantHandle
+    {
+        private readonly IEventPublisher _eventPublisher;
+
+        private readonly IJobAnalyzeBLL _jobAnalyzeBLL;
+
+        private const int _pageSize = 100;
+
+        public CloudFileAutoClearJob(ISysTenantDAL sysTenantDAL, IEventPublisher eventPublisher,
+            IJobAnalyzeBLL jobAnalyzeBLL) : base(sysTenantDAL)
+        {
+            this._eventPublisher = eventPublisher;
+            this._jobAnalyzeBLL = jobAnalyzeBLL;
+        }
+
+        public override async Task ProcessTenant(SysTenant tenant)
+        {
+            var myDate = DateTime.Now.AddDays(-1);
+            foreach (var itemFile in EmTenantCloudStorageType.TenantCloudStorageTypeTags)
+            {
+                _eventPublisher.Publish(new CloudFileAutoDelDayEvent(tenant.Id)
+                {
+                    DelDate = myDate,
+                    FileTag = itemFile.Tag
+                });
+            }
+        }
+    }
+}
