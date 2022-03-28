87d169c24e0e3477ec529fa70f010d2252253b3f
chenjian8541 <364905438@qq.com>
2022-03-28 17:42:28 +0800

    优化

---
+++ ETMS.Alien.Webapi/Core/ControllerExtensions.cs
@@ -0,0 +1,20 @@
+﻿using ETMS.Entity.Config;
+using ETMS.Utility;
+
+namespace ETMS.Alien.Webapi.Core
+{
+    public static class ControllerExtensions
+    {
+        /// <summary>
+        /// 员工信息
+        /// </summary>
+        /// <param name="this"></param>
+        /// <returns></returns>
+        public static Tuple<int, long, string> GetTokenInfo(this HttpRequest @this)
+        {
+            var tokenValue = @this.HttpContext.User?.Claims?.FirstOrDefault(p => p.Type.Equals(SystemConfig.AuthenticationConfig.ClaimType))?.Value;
+            var values = tokenValue.Split(',');
+            return Tuple.Create(values[1].ToInt(), values[2].ToLong(), values[3]);
+        }
+    }
+}
