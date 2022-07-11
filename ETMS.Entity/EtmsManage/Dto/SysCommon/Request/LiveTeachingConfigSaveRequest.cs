79a943675d42878e73221a7690a75f3b8ab2ed3c
13410271814 <chenjian8541@126.com>
2022-07-11 15:33:25 +0800

    产品培训配置

---
+++ ETMS.Entity/EtmsManage/Dto/SysCommon/Request/LiveTeachingConfigSaveRequest.cs
@@ -0,0 +1,24 @@
+﻿using ETMS.Entity.EtmsManage.Common;
+using ETMS.Entity.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETMS.Entity.EtmsManage.Dto.SysCommon.Request
+{
+    public class LiveTeachingConfigSaveRequest : AgentRequestBase
+    {
+        public LiveTeachingConfig config { get; set; }
+
+        public override string Validate()
+        {
+            if (config == null)
+            {
+                return "请输入配置信息"; 
+           }
+            return base.Validate();
+        }
+    }
+}
---
+++ ETMS.Entity/View/LiveTeachingConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETMS.Entity.View
+{
+    public class LiveTeachingConfig
+    {
+        public bool IsOpen { get; set; }
+
+        public List<LiveTeachingTimeRule> Rules { get; set; }
+
+        public string LiveTitle { get; set; }
+
+        public string LiveUrl { get; set; }
+
+        public string LiveNo { get; set; }
+
+        public string LivePwd { get; set; }
+    }
+
+    public class LiveTeachingTimeRule
+    {
+        public int Week { get; set; }
+
+        public string WeekDesc { get; set; }
+
+        public int StartTime { get; set; }
+
+        public string StartTimeDesc { get; set; }
+
+        public int EndTime { get; set; }
+
+        public string EndTimeDesc { get; set; }
+    }
+}
