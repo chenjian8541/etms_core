d2a42fa4f35477b2e57ae6fbcc813aec4d5da18a
陈一刀 <chenjian8541@126.com>
2022-10-11 18:54:01 +0800

    Update EtmsProcess.cs

--- Etms.Tools.Test/EtmsProcess.cs
+++ Etms.Tools.Test/EtmsProcess.cs
@@ -176,7 +176,7 @@ namespace Etms.Tools.Test
             register.UseSenparcWeixin(senparcWeixinSetting, (weixinRegister, registerService) =>
             {
                 weixinRegister.UseSenparcWeixinCacheCsRedis();
-                weixinRegister.RegisterWxOpenAccount(senparcWeixinSetting, "【小禾帮培训管理系统】小程序");
+                weixinRegister.RegisterWxOpenAccount(senparcWeixinSetting, "【】小程序");
                 weixinRegister.RegisterOpenComponent(senparcWeixinSetting,
                     async componentAppId =>
                     {
